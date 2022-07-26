a7e4ea86e180f05755e2c8eb78a5f3287affb54c
DevEticeo <107034564+DevEticeo@users.noreply.github.com>
2022-07-26 10:05:53 +0200

    Correction of the plugin
    Correction of the plugin after the Dan Marsden's coments

---
+++ glossaryfocus/classes/external.php
@@ -0,0 +1,105 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External API for glossaryfocus
+ *
+ * @package    mod_glossaryfocus
+ * @copyright  2021 Eticeo <https://eticeo.com>
+ * @author     2021 Jeremy Carre <jeremy.carre@eticeo.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
+ */
+namespace mod_glossaryfocus;
+defined('MOODLE_INTERNAL') || die();
+
+use external_api;
+use external_function_parameters;
+use external_value;
+use external_single_structure;
+use external_multiple_structure;
+
+require_once($CFG->libdir."/externallib.php");
+
+/**
+ * Class external
+ *
+ * @package    mod_glossaryfocus
+ * @copyright  2021 Eticeo <https://eticeo.com>
+ * @author     2021 Jeremy Carre <jeremy.carre@eticeo.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
+ */
+class external extends \external_api {
+    /**
+     * Returns glossaryfocus_get_words() parameters.
+     *
+     * @return \external_function_parameters
+     */
+    public static function get_words_parameters() {
+        return new \external_function_parameters(
+            array(
+                'query' => new external_value(PARAM_TEXT,
+                    'Query string (full or partial word name)'),
+                'idglossary' => new external_value(PARAM_INT, 'Glossary master id (0 if none)'),
+            )
+        );
+    }
+
+    /**
+     * Handles return the element's HTML.
+     *
+     * @param $query        | string|null 
+     * @param $idglossary   |  int|null 
+     *
+     * @return array of words
+     */
+    public static function get_words($query, $idglossary) {
+        global $DB;
+        
+        $result = [];
+        
+        $params = array('query' => '%'.$query.'%');
+        $condition ="";
+        if ($idglossary > 0) {
+            $condition .= " AND glossaryid = :glossaryid";
+            $params['glossaryid'] = $idglossary;
+        }
+        $listWords = $DB->get_records_sql("SELECT ge.id, ge.concept, g.name
+                                            FROM {glossary_entries} ge 
+                                            INNER JOIN {glossary} g ON ge.glossaryid = g.id AND g.globalglossary = 1
+                                            WHERE ".$DB->sql_like('concept', ':query')." ".$condition, $params);
+
+        foreach ($listWords as $word) {
+            $result[] = ['id' => $word->id, 'name' => $word->concept.' ('.$word->name.')'];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns the get_words result value.
+     *
+     * @return \external_value
+     */
+    public static function get_words_returns() {
+        return new external_multiple_structure(
+            new external_single_structure([
+                'id'    => new external_value(PARAM_INT, 'ID of the word'),
+                'name'  => new external_value(PARAM_NOTAGS, 'The word name')
+            ])
+        );
+    }
+}
